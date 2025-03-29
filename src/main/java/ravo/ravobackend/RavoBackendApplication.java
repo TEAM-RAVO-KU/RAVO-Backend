26a6ac4c0511952ea0d26ac5f13e31bb34dea25b
seongjunnoh <joon1754@naver.com>
2025-03-29 11:21:05 +0900

    스프링 프로젝트 init

---
+++ src/main/java/ravo/ravobackend/RavoBackendApplication.java
@@ -0,0 +1,13 @@
+package ravo.ravobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RavoBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RavoBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ravo/ravobackend/RavoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ravo.ravobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RavoBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
