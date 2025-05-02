73b9066949c74d17ae47e19ddd8a28184f41dcbb
kkb <gimgyubin1225@gmail.com>
2025-05-02 14:36:05 +0900

    [FEAT] batch job repository datasource 추가
    - active, standby db와 프로그램 실행 DB를 분리하기 위해 임시로 h2 db 정보를 yml에 추가하고 해당 db를 default로 사용하도록 하였음

--- src/main/java/ravo/ravobackend/global/config/DataSourceConfig.java
+++ src/main/java/ravo/ravobackend/global/config/DataSourceConfig.java
@@ -1,5 +1,6 @@
 package ravo.ravobackend.global.config;
 
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
@@ -11,15 +12,28 @@ import javax.sql.DataSource;
 @Configuration
 public class DataSourceConfig {
 
-    // liveDataSource 를 default dataSource로 등록
-    @Bean(name = {"dataSource", "liveDataSource"})
+    @Bean
+    @ConfigurationProperties("spring.datasource.batch")
+    public DataSourceProperties batchDataSourceProperties() {
+        return new DataSourceProperties();
+    }
+
+    @Bean(name = "dataSource")
     @Primary
+    public DataSource batchDataSource() {
+        return batchDataSourceProperties()
+                .initializeDataSourceBuilder()
+                .build();
+    }
+
+    // liveDataSource 를 default dataSource로 등록
+    @Bean
     @ConfigurationProperties(prefix = "spring.datasource.live")
     public DataSource liveDataSource() {
         return DataSourceBuilder.create().build();
     }
 
-    @Bean(name = "standbyDataSource")
+    @Bean
     @ConfigurationProperties(prefix = "spring.datasource.standby")
     public DataSource standbyDataSource() {
         return DataSourceBuilder.create().build();
